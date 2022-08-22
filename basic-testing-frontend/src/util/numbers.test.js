4da82095fc6b85772028f33069758cc62dcfdebf
Dariusz Okonski <darek200180@gmail.com>
2022-08-22 19:25:03 +0100

    add validation.js

--- basic-testing-frontend/src/util/numbers.test.js
+++ basic-testing-frontend/src/util/numbers.test.js
@@ -1,5 +1,5 @@
 import { it, expect } from 'vitest';
-import { transformToNumber } from './../../../basic-testing-nodejs-esmodules/src/util/numbers';
+import { transformToNumber } from './numbers';
 
 it('should transform a string number to a number of type number', () => {
     const input = '1';
@@ -12,8 +12,11 @@ it('should transform a string number to a number of type number', () => {
 
 it('should yield NaN for non-transformable values', () => {
     const input = 'invalid';
+    const input2 = {};
 
     const result = transformToNumber(input)
+    const result2 = transformToNumber(input2);
 
     expect(result).toBeNaN();
+    expect(result2).toBeNaN();
 })
