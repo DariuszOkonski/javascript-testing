581d2b1f8c887df9d8131a3c0d6f81151123e95e
Dariusz Okonski <darek200180@gmail.com>
2022-08-23 18:44:01 +0100

    refactor file app.js

--- basic-testing-frontend/src/parser.js
+++ basic-testing-frontend/src/parser.js
@@ -4,4 +4,10 @@ export function extractNumbers(formData) {
 
   return [num1Input, num2Input];
 }
+  
+export function extractEnteredNumberValues(form) {
+  const formData = new FormData(form);
+  const numberInputs = extractNumbers(formData);
+  return numberInputs
+}
 
