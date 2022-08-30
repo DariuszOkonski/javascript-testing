3956e82d115cd9192627a43b9d2c28c484311a89
Dariusz Okonski <darek200180@gmail.com>
2022-08-30 20:15:12 +0100

    add concurent to run a large amount of tests at the same time

--- basic-testing-frontend/async/async-example.test.js
+++ basic-testing-frontend/async/async-example.test.js
@@ -1,29 +1,31 @@
-import { it, expect } from 'vitest';
+import { it, expect, describe } from 'vitest';
 import { generateToken, generateTokenPromise } from './async-example';
 
-it('should generate a token value', (done) => {
-    const testUserEmail = 'test@test.com';
-
-    generateToken(testUserEmail, (err, token) => {
-        try {
-            expect(token).toBeDefined();
-            done()
-        } catch (error) {
-            done(error);            
-        }
+describe.concurrent('async-example', () => {
+    it('should generate a token value', (done) => {
+        const testUserEmail = 'test@test.com';
+        
+        generateToken(testUserEmail, (err, token) => {
+            try {
+                expect(token).toBeDefined();
+                done()
+            } catch (error) {
+                done(error);            
+            }
+        });
     });
-});
-
-it('should generate a token value', () => {
-    const testUserEmail = 'test@test.com';
-
-    return expect(generateTokenPromise(testUserEmail)).resolves.toBeDefined();
-});
-
-it('should generate a token value version await async', async () => {
-    const testUserEmail = 'test@test.com';
-
-    const result = await generateTokenPromise(testUserEmail);
-
-    expect(result).toBeDefined();
+    
+    it('should generate a token value', () => {
+        const testUserEmail = 'test@test.com';
+        
+        return expect(generateTokenPromise(testUserEmail)).resolves.toBeDefined();
+    });
+    
+    it('should generate a token value version await async', async () => {
+        const testUserEmail = 'test@test.com';
+        
+        const result = await generateTokenPromise(testUserEmail);
+        
+        expect(result).toBeDefined();
+    })
 })
